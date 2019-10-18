1f9f096ba15969546cc820242689203ed4bad360
danielmaartens <danielrmaartens@gmail.com>
2019-10-18 14:03:56 +0200

    completed image carousel; info banner; added premium ribbon; finished header.

--- card/src/components/Card.vue
+++ card/src/components/Card.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="card-container">
+
+
+
         <div class="card">
+
+
+
+            <div class="heading">
+                <img height="60px" width="50px" v-if="currentEstablishment.premium" class="premium-ribbon" src="@/assets/premium-ribbon.png">
+                <h2>{{currentEstablishment.name}}</h2>
+                <div class="tourism-grading" :style="cssVars" >
+
+                    <img :style="cssVars" v-for="index in currentEstablishment.tourismGrading" :key="`tgcsa--${index}`"
+                         src="@/assets/tgcsa.svg">
+                </div>
+            </div>
 
             <div class="carousel">
 
-
                 <agile :autoplay-speed="5000" :speed="2500" fade autoplay>
-                    <div class="slide" v-for="(image, index) in images" :key="`image--${index}`">
+                    <div class="slide" v-for="(image, index) in currentEstablishment.images" :key="`image--${index}`">
                         <img class="image" :src="image" :alt="`image--${index}`">
                     </div>
                     <template v-slot:prevButton>
@@ -19,6 +33,44 @@
                 </agile>
             </div>
 
+            <div class="info-container">
+
+                <div class="info-icons left">
+                    <div class="icon-left">
+                        <div class="bed-count">3</div>
+                        <img class="icon" src="@/assets/double-bed.svg">
+                    </div>
+
+                    <div class="icon-left">
+                        <div class="bed-count">2</div>
+                        <img class="icon icon-left" src="@/assets/single-bed.svg">
+                    </div>
+
+                    <div class="icon-left">
+
+                        <div>$$$</div>
+                        <div class="price-container">
+                            <div>from</div>
+                            <div class="price"><b>R10</b></div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+                <div class="view-more">
+                    <div class="view-more-button">
+                        view more
+                    </div>
+
+                </div>
+                <div class="info-icons right">
+                    <img class="icon icon-right" src="@/assets/user-rating-star.svg">
+                    <img class="icon icon-right" src="@/assets/review-icon.svg">
+                </div>
+
+            </div>
+            <p>{{currentEstablishment.shortDescription}}</p>
         </div>
     </div>
 </template>
@@ -33,15 +85,12 @@
         },
         computed: {
             ...mapState([
-                    'images',
-                    'imageHeight',
-                    'imageWidth'
+                    'currentEstablishment',
                 ],
             ),
             cssVars() {
                 return {
-                    '--height': this.imageHeight + 'px',
-                    '--width': this.imageWidth + 'px',
+                    '--grading-margin': (this.currentEstablishment.premium ? 32 : 10) + 'px',
                 }
             }
         },
@@ -50,6 +99,62 @@
 </script>
 
 <style lang="scss">
+
+    $cardWidth: 500px;
+    $carouselHeight: 300px;
+
+    @function debug($list, $type: true, $root: true) {
+        $line-break: "\A ";
+        $result: if($type,
+                "(list:#{length($list)})[ "+ $line-break,
+                "[ " + $line-break
+        );
+        $space: if($root,
+                "",
+                "  "
+        );
+
+        @each $item in $list {
+            $result: $result + "  ";
+
+            @if length($item) > 1 {
+                $result: $result + debug($item, $type, false);
+            }
+
+            @else {
+                $result: if($type,
+                        $result + $space + "(" + type-of($item) + ") " + $item,
+                        $result + $space + $item
+                );
+            }
+
+            @if index($list, $item) != length($list) {
+                $result: $result + ", " + $line-break;
+            }
+        }
+
+        $result: $result + $line-break + $space + "]";
+        @return quote($result);
+    }
+
+    @mixin debug($list) {
+        body:before {
+            content: debug($list) !important;
+
+            display: block !important;
+            margin: 1em !important;
+            padding: 0.5em !important;
+
+            background: #efefef !important;
+            border: 1px solid #ddd !important;
+            border-radius: 0.2em !important;
+
+            color: #333 !important;
+            font: 0.75em/1.5 'Courier New', monospace !important;
+            text-shadow: 0 1px white !important;
+            white-space: pre-wrap !important;
+        }
+    }
 
     .card-container {
         display: block;
@@ -60,13 +165,131 @@
 
     .card {
         height: 800px;
-        width: 500px;
+        width: $cardWidth;
         border: 1px solid black;
         margin: 0 auto;
+
+        p {
+            margin: 10px;
+        }
+    }
+
+    .heading {
+        position: absolute;
+        top: 0;
+        width: $cardWidth;
+        height: 5%;
+        background-color: rgba(black, .5);
+        color: white;
+        z-index: 1;
+        text-align: left;
+
+        h2 {
+            position: absolute;
+            bottom: 0;
+            margin: 0 0 0 5px;
+            color: white;
+            float: left;
+        }
+
+        div {
+
+        }
+    }
+
+    .premium-ribbon {
+        position: absolute;
+        z-index: 1;
+        right: -22px;
+        top: -20px;
+    }
+
+    .tourism-grading {
+        float: right;
+        height: 100%;
+        margin-right: var(--grading-margin);
+
+        img {
+            width: 20px;
+            height: 100%;
+            vertical-align: middle;
+            margin: {
+                left: 5px;
+                top: 5px;
+            }
+        }
+    }
+
+    .info-container {
+        width: 100%;
+        height: 50px;
+        background-color: #dddddd;
+        border: 1px solid black;
+        display: flex;
+        flex-direction: row;
+
+        div {
+            flex: .333;
+            vertical-align: middle;
+        }
+    }
+
+    .icon {
+        height: 30px;
+        width: 30px;
+
+        &-left {
+
+                display: inline-block;
+                margin: 0 5px 0 5px;
+
+        }
+
+        &-right {
+            padding: 6% 0;
+            margin: 0 10px 0 10px;
+        }
+
+    }
+
+    .info-icons {
+
+    }
+
+    .bed-count {
+        position: relative;
+        top: 5px;
+    }
+
+    .price-container {
+        font-size: 10px;
+        position: relative;
+    }
+
+    .price {
+        position: relative;
+        bottom: 3px;
+    }
+
+
+
+    .view-more {
+        margin-top: auto;
+        height: 60%;
+        color: white;
+        background-color: black;
+        border-radius: 5px 5px 0 0;
+        display: table;
+
+    }
+
+    .view-more-button {
+        display: table-cell;
+        vertical-align: middle;
     }
 
     .agile {
-        height: 300px;
+        height: $carouselHeight;
     }
 
     .agile__nav-button {
@@ -128,7 +351,7 @@
 
     .agile .slide {
         display: block;
-        height: 300px;
+        height: $carouselHeight;
         object-fit: cover;
         width: 100%;
     }
@@ -137,7 +360,7 @@
         overflow: hidden;
         min-width: 100%;
         width: 0;
-        height: 300px;
+        height: $carouselHeight;
     }
 
     .agile__actions {
@@ -148,8 +371,8 @@
     }
 
     .image {
-        width: 500px;
-        height: 300px;
+        width: $cardWidth;
+        height: $carouselHeight;
     }
 
     .chevron-left {
--- card/src/store/index.js
+++ card/src/store/index.js
@@ -10,21 +10,30 @@ export default new Vuex.Store({
   state: {
     imageHeight: IMAGE_HEIGHT,
     imageWidth: IMAGE_WIDTH,
-    images: [
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDc2NDdjZWE5LTM4YWQtNDY4Yy05MmEzLWI3MDM1MTdmYWE2Yw?fit=crop&w=638&h=440&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDg0MzkzZjQ5LTg0OWItNDA0Ni1hZjEwLTg4YzMzNTE3ZmEwMw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGNiN2JmYTllLTNhMDYtNDE1ZS1iMmMyLTZmYjViZWUwODk5Mg?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGI2ZThkNmE5LWRmOWUtNDEwYi1hZjI5LTM4OTNkYzc2MmE3NA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDE5M2I2ODQ5LTE2YTItNGVjMS1hMjJlLWYwOGIwOGZhODFjZA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDBkM2I5ZWU4LTU3ZmQtNDMwZS1iNTc5LTY3NGM5NGY1NWZmNw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDJhYzdiZTkxLWNmODUtNDI3ZS1hNTc5LTA2ZTA3MjQ4MWE0NA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDNlYzM3ZTlhLWI3NjctNDc0MS04YzY4LWZkZTQ1MDkyMjMzMA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDk5ZTg5ZGJjLWZjMzQtNGJlZC1iYzRlLTZlNzI1Mjk1MmJlMw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGRjODQ3YzdhLWE1MzMtNGM3MC04NmEyLWM3YzUyYTA2MDFjMQ?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGU1YWVlMDRkLWY4NjEtNGY5OC04ZWQxLTdiMmY2MGZhYjhlZQ?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDVhZTAyYjUwLTBkODctNGE0OS1hMDA1LWVjN2Y0NDMyMGVjOA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-      'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGJhMzMzMDM5LTJiODItNDE1Mi04YzM5LWQ5YzJmOWEzY2MzNA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
-    ],
+    currentEstablishment: {
+      name: 'Tsala Treetop Lodge',
+      shortDescription: 'Dié lodge se toweragtige ligging is onweerstaanbaar vir diegene wat op soek is na eksklusiewe verblyf.',
+      tourismGrading: 5,
+      averageUserRating: {
+        overall: 0.8
+      },
+      premium: true,
+      images: [
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDc2NDdjZWE5LTM4YWQtNDY4Yy05MmEzLWI3MDM1MTdmYWE2Yw?fit=crop&w=638&h=440&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDg0MzkzZjQ5LTg0OWItNDA0Ni1hZjEwLTg4YzMzNTE3ZmEwMw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGNiN2JmYTllLTNhMDYtNDE1ZS1iMmMyLTZmYjViZWUwODk5Mg?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGI2ZThkNmE5LWRmOWUtNDEwYi1hZjI5LTM4OTNkYzc2MmE3NA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDE5M2I2ODQ5LTE2YTItNGVjMS1hMjJlLWYwOGIwOGZhODFjZA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDBkM2I5ZWU4LTU3ZmQtNDMwZS1iNTc5LTY3NGM5NGY1NWZmNw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDJhYzdiZTkxLWNmODUtNDI3ZS1hNTc5LTA2ZTA3MjQ4MWE0NA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDNlYzM3ZTlhLWI3NjctNDc0MS04YzY4LWZkZTQ1MDkyMjMzMA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDk5ZTg5ZGJjLWZjMzQtNGJlZC1iYzRlLTZlNzI1Mjk1MmJlMw?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGRjODQ3YzdhLWE1MzMtNGM3MC04NmEyLWM3YzUyYTA2MDFjMQ?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGU1YWVlMDRkLWY4NjEtNGY5OC04ZWQxLTdiMmY2MGZhYjhlZQ?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJDVhZTAyYjUwLTBkODctNGE0OS1hMDA1LWVjN2Y0NDMyMGVjOA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+        'https://travelground.imgix.net/AAEAAQAAAAAAAAAAAAAAJGJhMzMzMDM5LTJiODItNDE1Mi04YzM5LWQ5YzJmOWEzY2MzNA?fit=crop&w=1200&h=800&bg=000000&auto=enhance,compress&q=80',
+      ],
+    }
   },
   mutations: {
   },
